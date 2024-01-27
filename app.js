4c07a638900b38c1fb884bf6b4d55b0c7c7531a7
Pratik Devnal <79634014+pratikdevnal@users.noreply.github.com>
2024-01-27 10:54:29 +0530

    env added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
 // Define a route that responds with "Hello, World!" when accessed
 app.get("/", (req, res) => {
